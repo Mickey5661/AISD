69d76d6af991ee3fd85da021b39404d98cab2674
Marik368418 <117970735+Marik368418@users.noreply.github.com>
2025-04-06 17:32:37 +0300

    Lab 3 changes and review

--- FirstSemester/Lab3/Task2.py
+++ FirstSemester/Lab3/Task2.py
@@ -7,11 +7,11 @@ def generate_worst_case(n):
         return [1] + list(range(n, 1, -1))
 
 
-with open('input.txt', 'r') as file:
+with open('input2.txt', 'r') as file:
     n = int(file.readline().strip())
 
 # Генерация наихудшей перестановки
 worst_case = generate_worst_case(n)
 
-with open('output.txt', 'w') as file:
+with open('output2.txt', 'w') as file:
     file.write(' '.join(map(str, worst_case)))
--- FirstSemester/Lab3/Task7.py
+++ FirstSemester/Lab3/Task7.py
@@ -1,16 +1,50 @@
-def radix_sort(strings, n, m, k):
-    indices = list(range(n))
+def radix_sort(strings, k):
+    # Создаем список для хранения строк вместе с их индексами
+    indexed_strings = [(s, i) for i, s in enumerate(strings)]
+
+    # Проведение k фаз цифровой сортировки
     for phase in range(k):
-        indices.sort(key=lambda x: strings[m - phase - 1][x])
-    return indices
+        # Создаем списки для каждой буквы алфавита
+        buckets = [[] for _ in range(26)]
 
-# Чтение данных из input.txt
-with open("input.txt", "r") as f:
-    lines = f.readlines()
-    n, m, k = map(int, lines[0].strip().split())
-    strings = [list(line.strip()) for line in lines[1:m + 1]]
+        # Распределяем строки по спискам в зависимости от текущего символа
+        for s, idx in indexed_strings:
+            char_code = ord(s[-phase - 1]) - ord('a')
+            buckets[char_code].append((s, idx))
 
-sorted_indices = radix_sort(strings, n, m, k)
+        # Объединяем списки обратно в один
+        indexed_strings = []
+        for bucket in buckets:
+            indexed_strings.extend(bucket)
 
-with open("output.txt", "w") as f:
-    f.write(" ".join(map(str, [i + 1 for i in sorted_indices])))  # Индексы строк начинаются с 1
+    # Возвращаем индексы строк в отсортированном порядке
+    return [idx for _, idx in indexed_strings]
+
+
+def read_input(filename):
+    with open(filename, 'r') as file:
+        n, m, k = map(int, file.readline().split())
+
+        # Читаем строки по вертикали
+        strings = [''] * n
+        for i in range(m):
+            line = file.readline().strip()
+            for j in range(n):
+                strings[j] += line[j]
+
+    return n, m, k, strings
+
+
+def write_output(filename, indices):
+    with open(filename, 'w') as file:
+        file.write(' '.join(map(lambda x: str(x + 1), indices)))
+
+
+def main():
+    n, m, k, strings = read_input('input7.txt')
+    sorted_indices = radix_sort(strings, k)
+    write_output('output7.txt', sorted_indices)
+
+
+if __name__ == '__main__':
+    main()
